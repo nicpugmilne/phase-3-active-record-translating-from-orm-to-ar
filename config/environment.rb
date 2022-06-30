5c5872ff003dcc0ec59148564c4b992cd063c7ae
Nicola Milne <nicolalouisemilne@gmail.com>
2022-06-30 12:32:10 -0600

    commit tree 15c2aa3d24644b32aee7f0667dca6729e103666e

--- config/environment.rb
+++ config/environment.rb
@@ -1,12 +1,24 @@
 ENV["RACK_ENV"] ||= "development"
 
-require 'bundler/setup'
+require "bundler/setup"
 Bundler.require(:default, ENV["RACK_ENV"])
-require 'active_record'
+require "active_record"
 
-require_relative '../lib/dog'
+require_relative "../lib/dog"
 
 ActiveRecord::Base.establish_connection(
   adapter: "sqlite3",
   database: "./db/test.sqlite3"
 )
+
+# sql = <<-SQL
+#   CREATE TABLE IF NOT EXISTS dogs (
+#     id INTEGER PRIMARY KEY,
+#     name TEXT,
+#     breed TEXT
+#   )
+# SQL
+# ActiveRecord::Base.connection.execute(sql)
+
+# # Log SQL output to the terminal
+# ActiveRecord::Base.logger = Logger.new(STDOUT)
